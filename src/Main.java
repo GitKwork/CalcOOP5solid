61e1965acf678c6f9426765964bdb9a2f7d15f36
GitKwork <99908415+gitkwork@users.noreply.github.com>
2022-10-03 17:16:11 +0200

    Add refactoring

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws IllegalAccessException {
+        Presenter presenter = new Presenter();
+        presenter.start();
+    }
+}
+
+
